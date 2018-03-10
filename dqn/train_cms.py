bf2d0bb8013131e2b5165b92dbf0e4a89a62f8dc
Ryan <zcabrdc@ucl.ac.uk>
2018-03-10 21:44:40 +0000

    Added dqn code.

---
+++ dqn/train_cms.py
@@ -0,0 +1,23 @@
+import gym
+
+from baselines import deepq
+
+def main():
+    env = gym.make("cms-v0")
+    model = deepq.models.mlp([64], layer_norm=True)
+    act = deepq.learn(
+        env,
+        q_func=model,
+        lr=1e-3,
+        max_timesteps=100000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.1,
+        print_freq=10,
+        param_noise=True
+    )
+    print("Saving model to cms_model.pkl")
+    act.save("cms_model.pkl")
+
+if __name__ == '__main__':
+    main()
