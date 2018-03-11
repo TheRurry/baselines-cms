c24b96f78c32435031a68e40f3a6b6b766b1a5cd
Ryan <zcabrdc@ucl.ac.uk>
2018-03-11 17:35:53 +0000

    I broke it.

--- dqn/train_cms.py
+++ dqn/train_cms.py
@@ -5,21 +5,21 @@ from baselines import deepq
 
 def main():
     env = gym.make("cms-v0")
-    # model = deepq.models.mlp([64], layer_norm=True)
     model = deepq.models.mlp([64])
     act = deepq.learn(
         env,
         q_func=model,
         lr=1e-3,
-        max_timesteps=10 * 10000,
+        max_timesteps=100000,
         buffer_size=50000,
         exploration_fraction=0.1,
-        exploration_final_eps=0.1,
+        exploration_final_eps=0.02,
         print_freq=10,
-        # param_noise=True
+        # callback=callback
     )
-    print("Saving model to cms_model.pkl")
-    act.save("cms_model.pkl")
+    print("Saving model to cartpole_model.pkl")
+    act.save("cartpole_model.pkl")
+
 
 if __name__ == '__main__':
     main()
